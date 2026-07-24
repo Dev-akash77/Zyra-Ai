cd0fd8c7bf649ebd98c64e506fd828cc641b61d8
Akash Biswas <akashrahul2006@gmail.com>
2026-07-24 12:36:38 +0530

    feat(ai): implement PDF file upload and text chunking pipeline

--- server/src/modules/ai/ai.service.ts
+++ server/src/modules/ai/ai.service.ts
@@ -1,43 +1,68 @@
-import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 import { ConfigService } from '@nestjs/config';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 @Injectable()
-export class AiService{
+export class AiService {
   private readonly llm: ChatGoogleGenerativeAI;
 
- constructor(private readonly configService: ConfigService){
-  this.llm = new ChatGoogleGenerativeAI({
-    model:"gemini-2.5-flash",
-    apiKey:this.configService.get<string>('GOOGLE_API_KEY'),
-    temperature:0.3,
-  });
- }
+  constructor(private readonly configService: ConfigService) {
+    this.llm = new ChatGoogleGenerativeAI({
+      model: 'gemini-2.5-flash',
+      apiKey: this.configService.get<string>('GOOGLE_API_KEY'),
+      temperature: 0.3,
+    });
+  }
 
-
-//  ! test templete
-async testLLM(question:string):Promise<string>{
-  try {
-    const prompt = ChatPromptTemplate.fromMessages([
-      [
-          'system', 
-          'You are a highly professional and technical AI assistant name ZYRA AI. Answer all questions directly, accurately, and without unnecessary conversational filler. Keep it concise.'
+  //  ! test templete
+  async testLLM(question: string): Promise<string> {
+    try {
+      const prompt = ChatPromptTemplate.fromMessages([
+        [
+          'system',
+          'You are a highly professional and technical AI assistant name ZYRA AI. Answer all questions directly, accurately, and without unnecessary conversational filler. Keep it concise.',
         ],
-        ['human', '{input}']
-    ]);
+        ['human', '{input}'],
+      ]);
 
-    const chain = prompt.pipe(this.llm);
-    
-    const answer = await chain.invoke({input:question});
+      const chain = prompt.pipe(this.llm);
 
-    return answer.content.toString();
-    
-  } catch (error) {
-    console.error('LLM Error:', error);
-      throw new InternalServerErrorException('LLM se response fetch karne mein error aayi.');
-  }  
-}
+      const answer = await chain.invoke({ input: question });
 
-  
+      return answer.content.toString();
+    } catch (error) {
+      console.error('LLM Error:', error);
+      throw new InternalServerErrorException(
+        'LLM se response fetch karne mein error aayi.',
+      );
+    }
+  }
+
+
+  // ! upload file
+  async parseAndChunkPdf(fileBuffer:Buffer){
+    try {
+     const blob = new Blob([new Uint8Array(fileBuffer)], { type: 'application/pdf' });  
+     
+     const loader = new PDFLoader(blob);
+      const docs = await loader.load();
+
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+      });
+
+      const chunk = await splitter.splitDocuments(docs);
+
+      return chunk;
+
+    } catch (error) {
+      console.error('PDF Chunking Error:', error);
+      throw new InternalServerErrorException(
+        'PDF parse ya chunk karne mein error aayi.');
+    }
+  }
 }
